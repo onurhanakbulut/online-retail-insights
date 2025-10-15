3ca71b53b35f308134989f6dcfce25a2f0a4c5b0
onurhanakbulut <onurhanakbulut1@hotmail.com>
2025-10-15 14:33:28 +0300

    Recall@Top10% & CrossValid 5-Fold

--- random_forest.py
+++ random_forest.py
@@ -1,6 +1,7 @@
 import sqlite3 
 import pandas as pd
 from pathlib import Path
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -59,8 +60,8 @@ rf = RandomForestClassifier(
     class_weight='balanced'
     )
 
+#Optimizer ile ama recall düştü churn kaçtı.
 # =============================================================================
-#optimizer ile ama recall düştü churn kaçtı.
 # rf = RandomForestClassifier(
 #     n_estimators = 100,
 #     max_depth = 8,
@@ -69,18 +70,41 @@ rf = RandomForestClassifier(
 #     random_state = 11,
 #     class_weight='balanced'
 #     )
+# 
 # =============================================================================
-
 rf.fit(x_train, y_train)
 
-#pred & probabilty
+#pred & probabilty------------------
 y_pred = rf.predict(x_test)
 y_proba = rf.predict_proba(x_test)[:,1]
 
 
-#class report
+#class report---------------
 print(classification_report(y_test, y_pred))
 print("ROC-AUC:", round(roc_auc_score(y_test, y_proba), 3))
+
+
+#recall@Top10%--------------------------------
+y_true = y_test.values
+
+threshold_index = int(len(y_proba) * 0.10)
+top_indices = np.argsort(y_proba)[-threshold_index:]
+
+top_churns = y_true[top_indices]
+recall_top10 = sum(top_churns) / sum(y_true)    # tüm churn'lerin yüzde kaçı top10'da
+precision_top10 = sum(top_churns) / len(top_churns) # top10'da kaç kişi gerçekten churn
+
+
+######################################################################################
+print(f"Recall@Top10%: {recall_top10:.2%}")
+print(f"Precision@Top10%: {precision_top10:.2%}")       ##Recall@Top10%: 30.09%
+                                                        ##Precision@Top10%: 100.00%
+# =============================================================================
+# Model, gerçek churn müşterilerinin %30’unu en riskli %10’luk grupta tespit edebiliyor.
+# ani kampanyayı sadece bu kitleye yöneltmek, tüm churn’lerin neredeyse yarısını kurtarma potansiyeli taşıyor.”
+# =============================================================================
+
+
 
 
 
@@ -108,7 +132,7 @@ print("ROC-AUC:", round(roc_auc_score(y_test, y_proba), 3))
 # shap.summary_plot(sv, x_test)
 # =============================================================================
 
-##PER FEATURE CHURN ETKİSİ[ÇOK İYİ]PARTIAL DEPENDENCE
+##PER FEATURE CHURN ETKİSİ[ÇOK İYİ]PARTIAL DEPENDENCE--------------------------------------
 
 from sklearn.inspection import PartialDependenceDisplay
 
@@ -119,7 +143,7 @@ plt.suptitle("Partial Dependence Plots (PDP) for Churn Model")
 plt.tight_layout()
 plt.show()
 
-##MODEL ÜZERİNDE FEATURELARIN ETKİSİ (ÇOK İYİ) PERMUTATION IMPORTANCE
+##MODEL ÜZERİNDE FEATURELARIN ETKİSİ (ÇOK İYİ) PERMUTATION IMPORTANCE-----------------------------
 from sklearn.inspection import permutation_importance
 
 result = permutation_importance(rf, x_test, y_test, n_repeats=10, random_state=11)
@@ -145,7 +169,7 @@ plt.show()
 
 
 
-#CONFUSION MATRIX & ROC CURVE
+#CONFUSION MATRIX & ROC CURVE-------------------------------------------------------------
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, RocCurveDisplay
 
 ConfusionMatrixDisplay.from_estimator(rf, x_test, y_test)
@@ -156,9 +180,25 @@ RocCurveDisplay.from_estimator(rf, x_test, y_test)
 plt.title("ROC Curve")
 plt.show()
 
+##################CROSS VALIDATION, 5 -FOLD--------------------------------------------------
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.metrics import make_scorer, roc_auc_score
 
 
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=11)
 
+auc_scores = cross_val_score(
+    RandomForestClassifier(n_estimators=100, max_depth=6, class_weight='balanced', random_state=11),
+    x,
+    y,
+    cv=cv,
+    scoring='roc_auc',
+    n_jobs=-1,
+    error_score='raise'
+)
 
+print("ROC-AUC Scores:", np.round(auc_scores, 3))
+print("Mean AUC:", np.round(np.mean(auc_scores), 3))
+print("Std:", np.round(np.std(auc_scores), 3))
 
 
